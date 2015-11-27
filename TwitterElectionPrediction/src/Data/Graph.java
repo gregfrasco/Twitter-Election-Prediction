fa2fcc25f30adec2328d721f2c84c3d4e9fd4cd3
Greg Frasco <frascog@wit.edu>
2015-11-26 22:02:52 -0500

    Added Dataset to Graph

--- TwitterElectionPrediction/src/Data/Graph.java
+++ TwitterElectionPrediction/src/Data/Graph.java
@@ -7,6 +7,9 @@ package Data;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
  *
@@ -19,6 +22,31 @@ public class Graph {
 
     public Graph(Politicain politicain) {
         this.politicain = politicain;
-        this.barChart = ChartFactory.createBarChart("CAR USAGE STATIStICS", "Category", "Score", dataset,PlotOrientation.VERTICAL, true, true, false);
+        this.barChart = ChartFactory.createBarChart("CAR USAGE STATIStICS", "Category", "Score", createDataset(),PlotOrientation.VERTICAL, true, true, false);
     }
+    
+    private CategoryDataset createDataset(){
+      final String demPos = "D+";        
+      final String demNeg = "D-";        
+      final String repPos = "R+";        
+      final String repNeg = "R-";              
+      final DefaultCategoryDataset dataset = new DefaultCategoryDataset( );  
+
+//      dataset.addValue( 1.0 , fiat , speed );        
+//      dataset.addValue( 3.0 , fiat , userrating );        
+//      dataset.addValue( 5.0 , fiat , millage ); 
+//      dataset.addValue( 5.0 , fiat , safety );           
+//
+//      dataset.addValue( 5.0 , audi , speed );        
+//      dataset.addValue( 6.0 , audi , userrating );       
+//      dataset.addValue( 10.0 , audi , millage );        
+//      dataset.addValue( 4.0 , audi , safety );
+//
+//      dataset.addValue( 4.0 , ford , speed );        
+//      dataset.addValue( 2.0 , ford , userrating );        
+//      dataset.addValue( 3.0 , ford , millage );        
+//      dataset.addValue( 6.0 , ford , safety );               
+
+      return dataset; 
+   }
 }
